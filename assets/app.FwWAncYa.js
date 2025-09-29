0f6eb8c5d895bc509d5f3bf038a6fd9d781599f5
NanChen042 <2060518839@qq.com>
2025-09-30 04:16:11 +0800

    Updates

--- assets/app.FwWAncYa.js
+++
@@ -1,7 +0,0 @@
-import{aD as o,aR as p,aS as u,aT as l,aU as c,aV as f,aW as d,aX as m,aY as h,aZ as A,a_ as g,aP as P,d as v,az as R,z as w,l as y,a$ as C,b0 as _,b1 as b,ak as D}from"./chunks/framework.EgPEzSX8.js";import{R as E}from"./chunks/theme.OHBTB_aD.js";function i(e){if(e.extends){const a=i(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const s=i(E),T=v({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=R();return w(()=>{y(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&C(),_(),b(),s.setup&&s.setup(),()=>D(s.Layout)}});async function S(){const e=O(),a=L();a.provide(u,e);const t=l(e.route);return a.provide(c,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),s.enhanceApp&&await s.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function L(){return h(T)}function O(){let e=o,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=P(()=>import(n),__vite__mapDeps([]))),o&&(e=!1),r},s.NotFound)}o&&S().then(({app:e,router:a,data:t})=>{a.go().then(()=>{p(a.route,t.site),e.mount("#app")})});export{S as createApp};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
